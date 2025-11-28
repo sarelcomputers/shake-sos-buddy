b40c7e1d0b1aea870b9a5e0a815f6f455eb73e53
diemiskruier777 <diemiskruier777@gmail.com>
2025-11-28 18:33:32 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/core';
 const config: CapacitorConfig = {
   appId: 'app.lovable.5cb83c0c55a84b36a981495f8680a735',
   appName: 'Alfa22 SOS',
-  webDir: 'dist',
-  server: {
-    url: 'https://5cb83c0c-55a8-4b36-a981-495f8680a735.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  webDir: 'dist'
       plugins: {
     KeepAwake: {
       enabled: true
