0c2964e33252b47cdda98cfa0ce0f03b1a4bc5f3
diemiskruier777 <diemiskruier777@gmail.com>
2025-11-28 08:55:49 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,14 +4,14 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.5cb83c0c55a84b36a981495f8680a735',
   appName: 'Alfa22 SOS',
   webDir: 'dist',
+  server: {
+    url: 'https://5cb83c0c-55a8-4b36-a981-495f8680a735.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  },
     plugins: {
     KeepAwake: {
       enabled: true
-     server: {
-    url: 'https://5cb83c0c-55a8-4b36-a981-495f8680a735.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-      },
-    SmsManager: {
+        SmsManager: {
       // No specific configuration needed for basic SMS sending
     },
     BackgroundRunner: {
