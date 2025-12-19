3ce5f2ee3d95bc308ecc8f583e60744d665f847d
sarelcomputers <sarelcomputers@gmail.com>
2025-12-19 10:10:09 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.5cb83c0c55a84b36a981495f8680a735',
   appName: 'Alfa22 SOS',
   webDir: 'dist',
-  server: {
-    url: 'https://5cb83c0c-55a8-4b36-a981-495f8680a735.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+ // server: {
+//    url: 'https://5cb83c0c-55a8-4b36-a981-495f8680a735.lovableproject.com?forceHideBadge=true',
+//    cleartext: true
+//  },
   plugins: {
     KeepAwake: {
       enabled: true
